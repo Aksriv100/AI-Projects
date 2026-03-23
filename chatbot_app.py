7d58988c85cebc12d611bf71315ddff9e809f123
Aakarshit Srivastava <136953274+Aksriv100@users.noreply.github.com>
2026-03-23 22:55:21 +0530

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -2,9 +2,8 @@
 import streamlit as st
 from openai import OpenAI
 import os
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-py
 st.title("AI Chatbot")
 
 user_input = st.text_input("You:")
